68e7b3208330dc839aef6fdebb2b049b61544bbc
Joseph S <joe.david64@btinternet.com>
2025-10-23 12:13:37 +0100

    Added initial data-loading code

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import os
+import numpy as np
+import pandas as pd
+
 def main():
-    print("Hello from gdsc-drugbank-search!")
+    db, g1, g2 = get_data()
+    if(db==None or g1==None or g2==None):
+        return
+    return
+
+def get_data():
+    db, g1, g2 = None, None, None
+    for filename in os.listdir("Data"):
+        if("gdsc1" in filename.lower()):
+            g1 = pd.read_excel(os.path.join("Data",filename))
+        elif("gdsc2" in filename.lower()):
+            g2 = pd.read_excel(os.path.join("Data",filename))
+        elif(filename.lower()=="full database.xml"):
+            db = os.path.join("Data",filename)
+    if(db==None or g1==None or g2==None):
+        errors = ""
+        if(db==None):
+            errors += "DrugBank, "
+        if(g1==None):
+            errors += "GDSC1, "
+        if(g2==None):
+            errors += "GDSC2, "
+        errors = errors[:-2]
+        print(f"Error: cannot find file for {errors}")
+        return None, None, None
+    return db, g1, g2
 
 
 if __name__ == "__main__":
