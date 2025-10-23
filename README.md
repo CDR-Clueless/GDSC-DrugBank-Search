23e512c15f37e176fe01c7fb1999994787c9a4dd
CDR_Clueless <joe.david64@btinternet.com>
2025-10-23 17:10:20 +0100

    Fixed data imports and started exploration with lxml

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Tool to find drugs tested within GDSC dataset, locate these drugs in the DrugBank dataset, and find genes associated with this drug in the DrugBank dataset
 
 Setup:
+    Run 'main.py' to set up relevant directories
     Download the datasets 'All drugs' from DrugBank (https://go.drugbank.com/releases/latest#full) - this will need a DrugBank academic account.
     Download the datasets 'GDSC1-dataset' and 'GDSC2-dataset' from the CancerRXgene website (https://www.cancerrxgene.org/downloads/bulk_download) - this should be freely available
-    Create a folder named 'Data' within the downloaded github repository. Move all downloaded files here, extract the DrugBank data, and delete the zip folder if desired.
+    Move all downloaded files to (github repo/)"Data/Raw Data", extract the DrugBank data here, and delete the zip folder if desired.
     Ensure the GDSC1 data file (e.g. GDSC1_fitted_dose_response_27Oct23.xlsx) has the word 'GDSC1' in its name, ensure the GDSC2 data file has 'GDSC2' in its name, and ensure the DrugBank file is named 'full database.xml'
-    The main script can now be run
+    The main script should now run the comparison
--- main.py
+++ main.py
@@ -1,29 +1,44 @@
 import os
 import numpy as np
 import pandas as pd
+from lxml import etree
+
+from typing import Optional
 
 def main():
     db, g1, g2 = get_data()
-    if(db==None or g1==None or g2==None):
+    if(db is None or g1 is None or g2 is None):
         return
+    # The root tag is '{http://www.drugbank.ca}drugbank', while each subitem has the '{http://www.drugbank.ca}drug' tag
+    root = db.getroot()
+    print(root.tag)
+    for x in [root[0]]:
+        print(x.tag)
     return
 
-def get_data():
+def get_data() -> tuple[Optional[etree.ElementTree], Optional[pd.DataFrame], Optional[pd.DataFrame]]:
     db, g1, g2 = None, None, None
-    for filename in os.listdir("Data"):
+    if(os.path.exists("Data")==False):
+        os.mkdir("Data")
+        os.mkdir(os.path.join("Data","Raw Data"))
+    for filename in os.listdir(os.path.join("Data", "Raw Data")):
         if("gdsc1" in filename.lower()):
-            g1 = pd.read_excel(os.path.join("Data",filename))
+            g1 = pd.read_excel(os.path.join("Data", "Raw Data", filename))
+            print(f"Imported GDSC1 from {filename}")
         elif("gdsc2" in filename.lower()):
-            g2 = pd.read_excel(os.path.join("Data",filename))
+            g2 = pd.read_excel(os.path.join("Data", "Raw Data", filename))
+            print(f"Imported GDSC2 from {filename}")
         elif(filename.lower()=="full database.xml"):
-            db = os.path.join("Data",filename)
-    if(db==None or g1==None or g2==None):
+            with open(os.path.join("Data", "Raw Data", filename), "r") as f:
+                db = etree.parse(f)
+            print(f"Imported DrugBank from {filename}")
+    if(db is None or g1 is None or g2 is None):
         errors = ""
-        if(db==None):
+        if(db is None):
             errors += "DrugBank, "
-        if(g1==None):
+        if(g1 is None):
             errors += "GDSC1, "
-        if(g2==None):
+        if(g2 is None):
             errors += "GDSC2, "
         errors = errors[:-2]
         print(f"Error: cannot find file for {errors}")
