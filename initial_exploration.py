de1a790f39a6170abf3c3a757a9b9e89fbfc7575
CDR_Clueless <joe.david64@btinternet.com>
2025-12-04 16:24:25 +0000

    Initial code for drug-gene target pathfinding finished in initial_exploration.py

--- initial_exploration.py
+++ initial_exploration.py
@@ -33,8 +33,8 @@ DEFAULT_ALL_BY_ALL_FILE: str = os.path.join(CLEANED_DATA_DIR, "AllDrugsByAllGene
 DEFAULT_STRING_INFO_FILE: str = os.path.join(CLEANED_DATA_DIR, "9606.protein.info.v12.0.txt")
 DEFAULT_STRING_LINK_FILE: str = os.path.join(CLEANED_DATA_DIR, "9606.protein.links.v12.0.ssv")
 
-TARGET_DRUG: str = "965-D2"
-START_POINT_CUTOFF: float = 0.197
+#TARGET_DRUG: str = "965-D2"
+#START_POINT_CUTOFF: float = 0.197
 
 def gaussian(x, A, mu, sigma):
     return A*np.exp(-np.divide(np.power(x-mu, 2),(2*np.power(sigma, 2))))
@@ -44,11 +44,10 @@ def bimodal(x, A1, mu1, sigma1, A2, mu2, sigma2):
 
 def main():
 
-    CorrelationPlotter().plot_all()
-    return
+    #CorrelationPlotter().plot_all()
+    #return
     
 
-    """
     ## Import relevant datasets and amend them
     # HGNC
     hgnc = pd.read_table(DEFAULT_HUGO_FILE, low_memory=False).fillna('')
@@ -58,7 +57,7 @@ def main():
 
     # STRING proteins
     stinfo =  pd.read_table(DEFAULT_STRING_INFO_FILE,
-                            usecols=['#string_protein_id','preferred_name']).fillna('')g 
+                            usecols=['#string_protein_id','preferred_name']).fillna('')
     stinfo.rename(columns = {'#string_protein_id':'ID','preferred_name':'symbol'},inplace=True)
     stinfo = update_hgnc(stinfo, hgnc)
 
@@ -76,24 +75,89 @@ def main():
     allbyall = update_hgnc(allbyall, hgnc)
     allbyall = allbyall.set_index("symbol")
 
+    # DataFrame for the gene/protein targets of all drugs
+    drugTargets = pd.read_csv(os.path.join(CLEANED_DATA_DIR, "TargetRanking.tsv"), sep = "\t")
+    drugTargets = update_hgnc(drugTargets, hgnc, "TARGET")
+
+    # Dictionary of results for drug-gene survivability distributions
+    with open(os.path.join("Data", "Results", "Drug-gene correlation frequency histograms", "stats.json"), "r") as f:
+        drugGeneSurv = json.load(f)
+
+    ## Prepare graphs
     # Refine STRING links to those with a combined score >0.8
     stlink[stlink.combined_score.gt(800)]
     
     print('Building human link graph ...')
-    g = ig.Graph.TupleList(stlink.itertuples(index=False), directed=True, weights=False, edge_attrs="combined_score")
+    g_global = ig.Graph.TupleList(stlink.itertuples(index=False), directed=True, weights=False, edge_attrs="combined_score")
     print('Done !!')
-    g.vs["survivability"] = [allbyall[TARGET_DRUG].loc[gn] if gn in allbyall[TARGET_DRUG].index else float("NaN") for gn in g.vs["name"]]
-    print(ig.summary(g))
-    startPoints = [n for s,n in zip(g.vs["survivability"], g.vs["name"]) if s > START_POINT_CUTOFF]
-    print(startPoints)
-    shortest = np.inf
-    for sp in startPoints:
-        shortpath = g.get_shortest_paths(sp, to="MKNK1", weights=g.es["combined_score"], output="vpath")[0]
-        shortest = min(shortest, len(shortpath)-1)
+    # Make path for temporary data storage if none exists
+    tdpfp: str = os.path.join("Data", "Results", "drug_path_temp")
+    if(os.path.exists(tdpfp)==False):
+        os.mkdir(tdpfp)
+    for drug in tqdm(allbyall.columns, desc = "Calculating shortest drug paths"):
+        # Check if data for this drug already exists
+        if(os.path.exists(os.path.join(tdpfp, f"{drug}.json"))):
+            print(f"Found drug path data already calculated for {drug}; skipping...")
+            continue
+        # Get the appropriate threshold for 'starting point' genes
+        survivability_cutoff = get_drug_threshold(drug, drugGeneSurv, np.array(allbyall[drug].values, dtype = float))
+        # Get drug targets and save results output
+        targets = drugTargets.loc[drugTargets["DRUG"]==drug]["TARGET"].values
+        for target in targets:
+            g = deepcopy(g_global)
+            g.vs["survivability"] = [allbyall[drug].loc[gn] if gn in allbyall[drug].index else float("NaN") for gn in g.vs["name"]]
+            startPoints = [n for s,n in zip(g.vs["survivability"], g.vs["name"]) if s > survivability_cutoff]
+            shortest, shortestpath = np.inf, []
+            for sp in startPoints:
+                shortpath = g.get_shortest_paths(sp, to=target, weights=g.es["combined_score"], output="vpath")[0]
+                if(shortest>len(shortpath)):
+                    shortestpath = deepcopy(shortpath)
+                    shortest = len(shortpath)
+            # Get the nodes in this shortest path
+            pathnodes = [g.vs[v] for v in shortestpath]
+            with open(os.path.join(tdpfp, f"{drug}.json"), "w") as f:
+                json.dump({"path length": len(pathnodes)-1,
+                           "path": \
+                            {f"Path node {i}": {"name": pathnodes[i].attributes()["name"],
+                                    "survivability": pathnodes[i].attributes()["survivability"]} \
+                                            for i in range(len(pathnodes))}},
+                          f)
+    
+    ## Combine the per-drug jsons into a single, human-readable json and delete the originals
+    # Combine per-drug jsons
+    jsonContents = {}
+    for filename in os.listdir(tdpfp):
+        filedir = os.path.join(tdpfp, filename)
+        drug = filedir.replace(".json","")
+        with open(filedir, "r") as f:
+            drugDict = json.load(f)
+        jsonContents[drug] = deepcopy(drugDict)
+    # Save results to json
+    with open(os.path.join("Data", "Results", "drug paths.json"), "w") as f:
+        json.dump(jsonContents, f, indent = 4)
+    # Delete constituent parts
+    for filename in os.listdir(tdpfp):
+        filedir = os.path.join(tdpfp, filename)
+        os.remove(filedir)
+    os.rmdir(tdpfp)
     
     #USE HGNC ON DRUGBANK COMPARISON OUTPUT AND EXTEND SHORTEST PATHFINDING TO ALL TARGETS
-    """
     return
+
+def get_drug_threshold(drug: str, drugSurvivabilityDict: dict, survivability_array: Optional[np.ndarray]) -> float:
+    # Try to use the available data to get the appropriate cutoff
+    try:
+        rel = drugSurvivabilityDict[drug]
+        # If the modality is unimodal or unclear, use 3 SDs above the mean as the threshold
+        if(rel["modality details"]["modality"]!="bimodal"):
+            return rel["modality details"]["mean"] + rel["standard deviations"]["3.0"]
+        # If the modality is bimodal, use the mean of the higher curve survivability as the threshold
+        else:
+            return rel["curve parameters"]["mu1"]
+    # If unsuccessful, try to just use 3 SDs above the mean
+    except Exception as e:
+        print(f"Failed to retrieve modality information for {drug}; defaulting to 3SDs above norm...")
+        return float(np.mean(survivability_array)) + (float(np.std(survivability_array))*3.)
 
 if __name__ == "__main__":
     main()
