b742415757ba841c8a82e31bf23932cd060fd647
Joe (Work laptop) <joe.david64@btinternet.com>
2026-08-27 17:20:40 +0100

    Added initial stats testing to DAVID analysis

--- DAVID_analysis.py
+++ DAVID_analysis.py
@@ -8,6 +8,7 @@ Created 23 Jul 2026
 
 import os
 import numpy as np
+from scipy.stats import chi2_contingency
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -17,10 +18,15 @@ DAVID_DIR: str = os.path.join("Data", "Results", "DAVID-Analysis", "Downloads")
 OUTPUT_DIR: str = os.path.join("Data", "Results", "DAVID-Analysis")
 
 def main():
+    plot_david(saveDir = "", show_graph=False)
+
+def plot_david(saveDir: str = "", show_graph: bool = True):
     fig, ax = plt.subplots(layout='constrained', nrows = 3, ncols=2, figsize = (12.8, 9.6))
     k = 0
 
     keys = {"Tissue": "tissue", "Interations": "interaction", "Pathways": "pathway", "Protein Domains": "protein", "Transcription Factors": "transcription"}
+
+    statsResults: dict = {}
 
     for i in range(3):
         for j in range(2):
@@ -34,14 +40,16 @@ def main():
 
             p, pNon = import_david(targType="predicted", dtype = key), import_david(targType="non-predicted", dtype = key)
 
-            # Combine the two DataFrames into one with 3 Columns: Full Term, Proportion, Non-Predicted Proportion
-            combined = p[["Full Term", "Proportion"]]
-            combined = combined.merge(pNon[["Full Term", "Proportion"]], left_on="Full Term", right_on="Full Term")
-            combined.rename(columns={"Full Term": title, "Proportion_x": "Predicted Proportion", "Proportion_y": "Non-Predicted Proportion"}, inplace=True)
+            # Combine the two DataFrames into one with 5 Columns: Full Term, Predicted Proportion, Predicted Count, Non-Predicted Proportion, Non-Predicted Count
+            combined = p[["Full Term", "Count", "Proportion"]]
+            combined = combined.merge(pNon[["Full Term", "Proportion", "Count"]], left_on="Full Term", right_on="Full Term")
+            combined.rename(columns={"Full Term": title, "Proportion_x": "Predicted Proportion", "Proportion_y": "Non-Predicted Proportion",
+                                     "Count_x": "Predicted Count", "Count_y": "Non-Predicted Count"}, inplace=True)
             toAdd = pNon.loc[~pNon["Full Term"].isin(combined[title])]
-            toAdd.rename(columns = {"Full Term": title, "Proportion": "Non-Predicted Proportion"}, inplace=True)
-            toAdd = toAdd[[title, "Non-Predicted Proportion"]]
-            toAdd["Predicted Proportion"] = [0.0 for _ in range(len(toAdd))]
+            toAdd = toAdd.rename(columns = {"Full Term": title, "Proportion": "Non-Predicted Proportion", "Count": "Non-Predicted Count"})
+            toAdd = toAdd[[title, "Non-Predicted Proportion", "Non-Predicted Count"]]
+            toAdd["Predicted Proportion"] = 0.0
+            toAdd["Predicted Count"] = 0
             combined = pd.concat([combined, toAdd])
 
             for column, offset in zip(["Predicted Proportion", "Non-Predicted Proportion"], [-0.2, 0.2]):
@@ -58,14 +66,25 @@ def main():
             rel.set_title(f"Target Proportions by {title} type")
             rel.legend(loc="upper right")
 
+            # Calculate chi2 contingency statistic between the two
+            valsReal = combined[["Predicted Count", "Non-Predicted Count"]].to_numpy(dtype = int).T
+            valsProp = combined[["Predicted Proportion", "Non-Predicted Proportion"]].to_numpy(dtype = float)
+
+
+            statsResults[title] = {"Counts": chi2_contingency(valsReal).pvalue, "Proportions": chi2_contingency(valsProp).pvalue}
             k += 1
 
-    plt.savefig("Test DAVID Output.png")
+    print(statsResults)
 
-
+    if(saveDir!=""):
+        plt.savefig(saveDir)
+    if(show_graph):
+        plt.show()
+    plt.clf()
+    plt.close()
     return
 
-def import_david(dtype: str = "tissue", targType: str = "predicted"):
+def import_david(dtype: str = "tissue", targType: str = "predicted") -> pd.DataFrame:
     dtype = dtype.lower().strip()
     targType = targType.lower().strip()
     # Determine appropriate file suffix for data type
@@ -89,9 +108,6 @@ def import_david(dtype: str = "tissue", targType: str = "predicted"):
     df = pd.read_csv(os.path.join(DAVID_DIR, f"DAVIDChartReport_{targP} Targets_{suffix}.csv"))
     df["Proportion"] = df["Count"] / df["List Total"]
     return df
-
-def import_tissue() -> Tuple[pd.DataFrame, pd.DataFrame]:
-    return pd.read_csv(os.path.join(DAVID_DIR, "DAVIDChartReport_Non-Predicted Targets_Tissues.csv")), pd.read_csv(os.path.join(DAVID_DIR, "DAVIDChartReport_Predicted Targets_Tissues.csv"))
 
 if(__name__=="__main__"):
     main()
